fdc872782b84ba709a6d7ddc1efa0b700a8e35ca
Kurt Morrow <kurt.morrow@gmail.com>
2021-03-08 19:58:40 -0800

    minor changes for deployment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: 'process.env.DATABASE_URL' || 'postgresql://postgres@localhost/bnr-app',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/bnr-app',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/bnr-app-test',
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
     EMAIL_API_KEY: process.env.EMAIL_API_KEY,
