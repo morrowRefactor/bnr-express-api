6b282f3aaef4490a176d3f32e0925359edcb0fe8
Kurt Morrow <kurt.morrow@gmail.com>
2021-03-08 19:41:53 -0800

    changes for deployment

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use('/api/contact-us', contactUsRouter);
 app.use('/api/site-text', siteTextRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+  res.send('Hello, world! 22')
 });
 
 app.use(function errorHandler(error, req, res, next) {
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: 'postgres://ldftbmkldjmoex:334845eb05def47766d190b37a7aeca3449ca6d3697cbe7a5ec782f0b31d05ec@ec2-3-222-127-167.compute-1.amazonaws.com:5432/de9dhtjqa3e1dq' || 'postgresql://postgres@localhost/bnr-app',
+    DATABASE_URL: 'process.env.DATABASE_URL' || 'postgresql://postgres@localhost/bnr-app',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/bnr-app-test',
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
     EMAIL_API_KEY: process.env.EMAIL_API_KEY,
