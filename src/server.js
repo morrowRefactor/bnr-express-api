5c3eff86a961fb8f150f589421a1fb210efa2aa5
Kurt Morrow <kurt.morrow@gmail.com>
2021-03-10 12:26:27 -0800

    changes for deployment

--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ const { PORT, DATABASE_URL } = require('./config');
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
-  ssl: true
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 app.set('db', db);
